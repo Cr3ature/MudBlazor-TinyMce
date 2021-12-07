1c7782d75e84ae34677cbc0a9473295b78eef132
David Vanderheyden <david.vanderheyden1@outlook.be>
2021-12-07 09:09:05 +0100

    Build example

---
+++ TinyMcePOC/TinyMcePOC/Client/Pages/Components/TextEditorComponent.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace TinyMcePOC.Client.Pages.Components
+{
+    public partial class TextEditorComponent : MudDebouncedInput<string>
+    {
+        private readonly string _apiKey = "ts8ahgwtonepf070o23a2qc05j3tum8l2sj9eozleav2kywf";
+
+        private readonly Guid _identifier = Guid.NewGuid();
+        private Dictionary<string, object>? _editorConf;
+
+        [Parameter] public bool Inline { get; set; }
+        [Parameter] public string? Label { get; set; }
+
+        protected override void OnInitialized()
+        {
+            _editorConf = new()
+            {
+                { "selector", $"div#{_identifier}" },
+                { "menubar", false },
+                { "plugins", "link lists image media code autoresize" },
+                { "toolbar", "styleselect | forecolor backcolor | bold italic strikethrough subscript superscript underline | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | link image media | code" },
+                { "height", 300 },
+                { "autoresize_bottom_margin", 50 },
+                { "media_live_embeds", true },
+                { "toolbar_sticky", true },
+                { "toolbar_sticky_offset", 64 },
+            };
+        }
+    }
+}
---
+++ TinyMcePOC/TinyMcePOC/Client/Pages/TinyMceDemo.razor.cs
@@ -0,0 +1,57 @@
+﻿namespace TinyMcePOC.Client.Pages
+{
+    public partial class TinyMceDemo
+    {
+        private List<SectionDemo>? Sections { get; set; }
+        private int Counter;
+
+        protected override Task OnInitializedAsync()
+        {
+            Sections = new List<SectionDemo>()
+            {
+                new SectionDemo(){ Id = Guid.NewGuid(), Content = "Content for section 1", Name = "Section 1"},
+                new SectionDemo(){ Id = Guid.NewGuid(), Content = "Content for section 2", Name = "Section 2"},
+                new SectionDemo(){ Id = Guid.NewGuid(), Content = "Content for section 3", Name = "Section 3"},
+                new SectionDemo(){ Id = Guid.NewGuid(), Content = "Content for section 4", Name = "Section 4"},
+            };
+
+            Counter = Sections.Count;
+
+            return base.OnInitializedAsync();
+        }
+
+        private void AddSection()
+        {
+            if (Sections == null) Sections = new();
+
+            Counter++;
+            Sections.Add(new SectionDemo
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Section {Counter}",
+                Content = $"Content for section {Counter}"
+            });
+            StateHasChanged();
+        }
+
+        private void RemoveSection(Guid sectionId)
+        {
+            if(Sections == null) return;
+
+            Sections.Remove(Sections.First(_ => _.Id == sectionId));
+            StateHasChanged();
+        }
+
+        private void DoSomething(SectionDemo section)
+        {
+            Console.WriteLine(section);
+        }
+    }
+
+    public class SectionDemo
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Content { get; set; }
+    }
+}
